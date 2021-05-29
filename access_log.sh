d0b983172776b8f1eadea5c0f4d920ee82922281
masayanakajima <nakajima1812@gmail.com>
2021-05-29 15:17:17 +0900

    iroiro

--- access_log.sh
+++ access_log.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec docker_web_1 alp --sum -f /var/log/nginx/access.log --aggregates="/icons/*","/history/*","/channel/*","/profile/*" --avg
+docker exec docker_web_1 alp ltsv --file=/var/log/nginx/access.log -m "/icons/*","/history/*","/channel/*","/profile/*" --sort=avg -o count,method,uri,avg,min,max
