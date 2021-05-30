3e878100e925e56b8d1c08422419a0765d870781
masayanakajima <nakajima1812@gmail.com>
2021-05-30 16:43:48 +0900

    slowlog削除

--- bench.sh
+++ bench.sh
@@ -2,5 +2,8 @@
 
 docker exec docker_web_1 rm /var/log/nginx/access.log
 docker exec docker_web_1 rm /var/log/nginx/error.log
+docker exec docker_web_1 rm /var/cache/nginx/*
+
 docker exec docker_web_1 nginx -s reload
+docker exec docker_db_1 cp /dev/null /var/log/mysql/slow.log
 docker-compose up bench
--- docker-restart.sh
+++ docker-restart.sh
@@ -5,4 +5,4 @@ docker stop docker_${NAME}_1
 docker rm docker_${NAME}_1
 docker images -a | grep isucon7-qualifier-${NAME} | awk '{print $3}' | xargs docker rmi
 docker builder prune --force
-docker-compose up -d ${NAME}
+docker-compose up ${NAME}
